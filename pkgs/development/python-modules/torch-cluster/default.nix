04dadd8c1dcfa0c383af3764b66a350cfc8fdee0
Ryan Swart <ryan@talo.systems>
2022-02-24 00:08:22 +0800

    change version to master

--- pkgs/development/python-modules/torch-cluster/default.nix
+++ pkgs/development/python-modules/torch-cluster/default.nix
@@ -7,8 +7,9 @@ in buildPythonPackage {
   version = "1.5.9";
   src = fetchurl {
     url =
-      "https://files.pythonhosted.org/packages/4b/27/2f38017ae386ba27c13efe6a87eca7c3c58766c3117c86e0fa2c3f7562a9/torch_cluster-1.5.9.tar.gz";
-    sha256 = "19fwb2k96j4bc6krbz2ds4i5crfqrp97vgqzx5zqwg077ygrw84n";
+      "https://github.com/rusty1s/pytorch_cluster/archive/refs/heads/master.tar.gz";
+    #"https://files.pythonhosted.org/packages/4b/27/2f38017ae386ba27c13efe6a87eca7c3c58766c3117c86e0fa2c3f7562a9/torch_cluster-1.5.9.tar.gz";
+    sha256 = "sha256-mjZALNY77Wy+3M7l8qkFADiZtI6ddA4BhXImT8m1C6Q=";
   };
   format = "setuptools";
   doCheck = false;
