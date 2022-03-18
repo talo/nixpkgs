7a448ae44406487a59c24988df3788a700a118b7
Ryan Swart <ryan@talo.systems>
2022-03-18 10:12:59 +0800

    fix: pin torch-cluster

--- pkgs/development/python-modules/torch-cluster/default.nix
+++ pkgs/development/python-modules/torch-cluster/default.nix
@@ -7,9 +7,9 @@ in buildPythonPackage {
   version = "1.5.9";
   src = fetchurl {
     url =
-      "https://github.com/rusty1s/pytorch_cluster/archive/refs/heads/master.tar.gz";
+      "https://github.com/rusty1s/pytorch_cluster/archive/refs/tags/1.5.9.tar.gz";
     #"https://files.pythonhosted.org/packages/4b/27/2f38017ae386ba27c13efe6a87eca7c3c58766c3117c86e0fa2c3f7562a9/torch_cluster-1.5.9.tar.gz";
-    sha256 = "sha256-mjZALNY77Wy+3M7l8qkFADiZtI6ddA4BhXImT8m1C6Q=";
+    sha256 = "sha256-GbRnGlGekdM0WlmxASrwBHN74+fCFFp9wcyuUp+a51M=";
   };
   format = "setuptools";
   doCheck = false;
