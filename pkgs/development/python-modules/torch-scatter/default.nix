8d8a4aea26896a523642d59ffeac826c0d99173f
Ryan Swart <ryan@talo.systems>
2022-06-03 14:44:19 +0800

    fix: ensure torch-scatter version matches

--- pkgs/development/python-modules/torch-scatter/default.nix
+++ pkgs/development/python-modules/torch-scatter/default.nix
@@ -1,13 +1,14 @@
-{ buildPythonPackage, cudatoolkit_11, fetchurl, torch-spline-conv, which
+{ buildPythonPackage, cudaPackages_11_5, fetchurl, torch-spline-conv, which
 , pytest-runner, pytorch-bin, ninja }:
 
+with cudaPackages_11_5;
 buildPythonPackage {
   pname = "torch-scatter";
   version = "2.0.9";
   preConfigure = ''
     export TORCH_CUDA_ARCH_LIST="8.0 8.0+PTX 8.6 8.6+PTX"
-    export CPATH=${cudatoolkit_11}/bin
-    export CUDA_HOME=${cudatoolkit_11}
+    export CPATH=${cudatoolkit}/bin
+    export CUDA_HOME=${cudatoolkit}
     export FORCE_CUDA="1"
     export MAX_JOBS=$NIX_BUILD_CORES
   '';
@@ -22,11 +23,11 @@ buildPythonPackage {
   enableParallelBuilding = true;
 
   buildInputs = [
-    cudatoolkit_11
+    cudatoolkit
 
     torch-spline-conv
   ];
-  nativeBuildInputs = [ which cudatoolkit_11 ninja ];
+  nativeBuildInputs = [ which cudatoolkit ninja ];
   checkInputs = [ ];
   #     buildHooks = "
   # LD_LIBRARY_PATH=$LD_LIBRARY_PATH:${stdenv.cc.cc.lib}/lib/
