c2b3a742ec8551d4013f596116107b5540495fc4
Ryan Swart <ryan@talo.systems>
2022-06-06 14:55:23 +0800

    feat: use wheel

--- pkgs/development/python-modules/torch-spline-conv/default.nix
+++ pkgs/development/python-modules/torch-spline-conv/default.nix
@@ -1,29 +1,32 @@
 { buildPythonPackage, fetchurl, cudaPackages_11_5, pytest-runner, pytorch-bin
 , which, ninja }:
 
-with cudaPackages_11_5;
+#with cudaPackages_11_5;
 buildPythonPackage {
   pname = "torch-spline-conv";
   version = "1.2.1";
+  format = "wheel";
   src = fetchurl {
     url =
-      "https://files.pythonhosted.org/packages/80/f8/4d010376565c59b3c397b1cf103edc4e9b2ed087c2bbd3677f0a92930d75/torch_spline_conv-1.2.1.tar.gz";
-    sha256 = "0f5lk8bcz1pr3dd3sqqsq52gq8p0acardhi8lxim4k6b1s76akrn";
+      "https://data.pyg.org/whl/torch-1.11.0%2Bcu115/torch_spline_conv-1.2.1-cp39-cp39-linux_x86_64.whl";
+    # url =
+    #   "https://files.pythonhosted.org/packages/80/f8/4d010376565c59b3c397b1cf103edc4e9b2ed087c2bbd3677f0a92930d75/torch_spline_conv-1.2.1.tar.gz";
+    sha256 = "sha256-vsOreU1ryWVTZ++NNG4/UjHOyWxwhwkqwFZbwR7XkLA=";
   };
-  preConfigure = ''
-    export TORCH_CUDA_ARCH_LIST="8.0 8.0+PTX 8.6 8.6+PTX"
-    export CPATH=${cudatoolkit}/include
-    export LD_LIBRARY_PATH=$''${cudatoolkit_11_5}/lib
-    export CUDA_HOME=${cudatoolkit}
-    export FORCE_CUDA="1"
-    export MAX_JOBS=$NIX_BUILD_CORES
-  '';
-  format = "setuptools";
+  #preConfigure = ''
+  #export TORCH_CUDA_ARCH_LIST="8.0 8.0+PTX 8.6 8.6+PTX"
+  #export CPATH=${cudatoolkit}/include
+  #export LD_LIBRARY_PATH=$''${cudatoolkit_11_5}/lib
+  #export CUDA_HOME=${cudatoolkit}
+  #export FORCE_CUDA="1"
+  #export MAX_JOBS=$NIX_BUILD_CORES
+  #'';
+  #format = "setuptools";
   doCheck = false;
   enableParallelBuilding = true;
-  buildInputs = [ cudatoolkit ];
+  #buildInputs = [ cudatoolkit ];
   #cudatoolkit_11_joined ];
   checkInputs = [ ];
-  nativeBuildInputs = [ pytest-runner which ninja ];
+  nativeBuildInputs = [ pytest-runner which ];
   propagatedBuildInputs = [ pytorch-bin ];
 }
