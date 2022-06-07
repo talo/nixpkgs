8f0715410cc0f17530990a925cded5c13c07c219
Ryan Swart <ryan@talo.systems>
2022-06-07 10:38:30 +0800

    fix: wheels for spline-conv and sparse

--- pkgs/development/python-modules/torch-sparse/default.nix
+++ pkgs/development/python-modules/torch-sparse/default.nix
@@ -7,24 +7,29 @@ python.pkgs.buildPythonPackage rec {
   version = "0.6.13";
   src = fetchurl {
     url =
-      "https://files.pythonhosted.org/packages/ee/44/b4a1d6d7fa67309781005c0e36723dd0bd745fc1a20c9330796055953b10/torch_sparse-0.6.13.tar.gz";
-    sha256 = "b4896822559f9b47d8b0186d74c94b7449f91db155a57d617fbeae9b722fa1f3";
+      "https://data.pyg.org/whl/torch-1.11.0%2Bcu115/torch_sparse-0.6.13-cp39-cp39-linux_x86_64.whl";
+    sha256 = "sha256-TR9F9ihZmNI3c4NKq1ICI9HcqqEZl1j8MM/jw5eiQcM=";
+    # url =
+    #   #"https://github.com/rusty1s/pytorch_sparse/archive/refs/heads/master.tar.gz";
+    #   "https://github.com/rusty1s/pytorch_sparse/archive/adc8be24bd391b3c61a8a44e655f585e12943893.tar.gz";
+    # #"https://files.pythonhosted.org/packages/ee/44/b4a1d6d7fa67309781005c0e36723dd0bd745fc1a20c9330796055953b10/torch_sparse-0.6.13.tar.gz";
+    # sha256 = "sha256-nwg8il9pLsk8K1gd2uJEPx36ux5aVeu+UCKlHYCDkYs=";
   };
-  makeFlags = [
-    "CUDA_HOME=${cudatoolkit}"
-    #"PREFIX=$(out)"
-  ];
+  # makeFlags = [
+  #   "CUDA_HOME=${cudatoolkit}"
+  #   #"PREFIX=$(out)"
+  # ];
   preConfigure = ''
     export TORCH_CUDA_ARCH_LIST="8.0 8.0+PTX 8.6 8.6+PTX"
     export CUDA_HOME=${cudatoolkit}
     export FORCE_CUDA="1"
     #export MAX_JOBS=$NIX_BUILD_CORES
   '';
-  format = "setuptools";
+  format = "wheel";
   doCheck = false;
   enableParallelBuilding = true;
-  buildInputs = [ which pytest-runner cudatoolkit ];
+  buildInputs = [ which pytest-runner cudatoolkit pytorch-bin ];
   checkInputs = [ ];
   nativeBuildInputs = [ which cudatoolkit ];
-  propagatedBuildInputs = [ scipy torch-spline-conv pytorch-bin ];
+  propagatedBuildInputs = [ scipy torch-spline-conv ];
 }
--- pkgs/development/python-modules/torch-spline-conv/default.nix
+++ pkgs/development/python-modules/torch-spline-conv/default.nix
@@ -1,24 +1,27 @@
-{ buildPythonPackage, fetchurl, cudaPackages_11_5, pytest-runner, pytorch-bin
+{ buildPythonPackage, fetchurl, cudaPackages_11_3, pytest-runner, pytorch-bin
 , which, ninja }:
 
-with cudaPackages_11_5;
+with cudaPackages_11_3;
 buildPythonPackage {
   pname = "torch-spline-conv";
   version = "1.2.1";
-  #format = "wheel";
-  format = "setuptools";
+  format = "wheel";
+  #format = "setuptools";
   src = fetchurl {
-    #url =
-    #  "https://data.pyg.org/whl/torch-1.11.0%2Bcu115/torch_spline_conv-1.2.1-cp39-cp39-linux_x86_64.whl";
     url =
-      "https://files.pythonhosted.org/packages/80/f8/4d010376565c59b3c397b1cf103edc4e9b2ed087c2bbd3677f0a92930d75/torch_spline_conv-1.2.1.tar.gz";
-    sha256 = "sha256-Nk9ljg7LTFJjpyjClhVT4CL8RMEaYz1aG/mGzxaatDg=";
+      "https://data.pyg.org/whl/torch-1.11.0%2Bcu113/torch_spline_conv-1.2.1-cp39-cp39-linux_x86_64.whl";
+    sha256 = "sha256-muH3slNLr2p2F6kJ5cHLNAWGT/LgeX5QmY2zVbcJmcI=";
+    # url =
+    #  "https://data.pyg.org/whl/torch-1.11.0%2Bcu115/torch_spline_conv-1.2.1-cp39-cp39-linux_x86_64.whl";
     #sha256 = "sha256-vsOreU1ryWVTZ++NNG4/UjHOyWxwhwkqwFZbwR7XkLA=";
+    # url =
+    #   "https://files.pythonhosted.org/packages/80/f8/4d010376565c59b3c397b1cf103edc4e9b2ed087c2bbd3677f0a92930d75/torch_spline_conv-1.2.1.tar.gz";
+    # sha256 = "sha256-Nk9ljg7LTFJjpyjClhVT4CL8RMEaYz1aG/mGzxaatDg=";
   };
-  makeFlags = [
-    "CUDA_HOME=${cudatoolkit}"
-    #"PREFIX=$(out)"
-  ];
+  # makeFlags = [
+  #   "CUDA_HOME=${cudatoolkit}"
+  #   #"PREFIX=$(out)"
+  # ];
   preConfigure = ''
     export TORCH_CUDA_ARCH_LIST="8.0 8.0+PTX 8.6 8.6+PTX"
     export CUDA_HOME=${cudatoolkit}
@@ -26,10 +29,9 @@ buildPythonPackage {
     #export MAX_JOBS=$NIX_BUILD_CORES
   '';
   doCheck = false;
-  enableParallelBuilding = true;
-  buildInputs = [ cudatoolkit ];
+  #enableParallelBuilding = true;
+  #buildInputs = [ cudatoolkit pytorch-bin ];
   #cudatoolkit_11_joined ];
   checkInputs = [ ];
   nativeBuildInputs = [ cudatoolkit pytest-runner which ];
-  propagatedBuildInputs = [ pytorch-bin ];
 }
