4cb3865a3c671357eab764799343a1b763b7a65b
Ryan Swart <ryan@talo.systems>
2022-07-21 17:07:31 +0800

    fix: don't build kernel

--- pkgs/applications/science/autodock-vina-gpu/default.nix
+++ pkgs/applications/science/autodock-vina-gpu/default.nix
@@ -1,25 +1,37 @@
-{ stdenv, boost177, git, cudatoolkit, ocl-icd, fetchurl }:
-
-  stdenv.mkDerivation rec {
-          pname = "Vina-GPU";
-          version = "main";
-          src = fetchurl {
-            url = "https://github.com/DeltaGroupNJUPT/Vina-GPU/archive/refs/heads/main.tar.gz";
-            sha256 =  "sha256-0J3NjVxKrmaoNC6iP0tL1JXQ/iZOJPHJr8Nenog91pQ=";
-          };
-          buildInputs =  [
-            boost177
-            git
-            ocl-icd
-            cudatoolkit
-          ];
-          buildPhase = ''
-tar -xf  ${boost177.src}
-make source -j12 OPENCL_LIB_PATH=${ocl-icd} BOOST_LIB_PATH=./boost_1_77_0
-          '';
-          installPhase = ''
-            mkdir -p $out/bin
-            ls >> $out/ls.txt
-            cp ./Vina-GPU $out/bin/
-          '';
-        }
+{
+  stdenv,
+  boost177,
+  git,
+  cudatoolkit,
+  ocl-icd,
+  fetchurl,
+  linuxPackages,
+  addOpenGLRunpath,
+}:
+stdenv.mkDerivation rec {
+  pname = "Vina-GPU";
+  version = "dev";
+  src = fetchurl {
+    url = "https://github.com/DeltaGroupNJUPT/Vina-GPU/archive/refs/heads/main.tar.gz";
+    sha256 = "sha256-0J3NjVxKrmaoNC6iP0tL1JXQ/iZOJPHJr8Nenog91pQ=";
+  };
+  nativeBuildInputs = [
+    ocl-icd
+    cudatoolkit
+  ];
+  buildInputs = [
+    boost177
+    git
+    ocl-icd
+    cudatoolkit
+  ];
+  buildPhase = ''
+    tar -xf  ${boost177.src}
+    make -j12 OPENCL_LIB_PATH=${ocl-icd} BOOST_LIB_PATH=./boost_1_77_0
+  '';
+  installPhase = ''
+    mkdir -p $out/bin
+    ls >> $out/ls.txt
+    cp ./Vina-GPU $out/bin/
+  '';
+}
