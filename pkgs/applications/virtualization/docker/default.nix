f736ca0d8e05325c262aec6fb6b6fb4ef294bd51
Ryan Swart <ryan@talo.systems>
2022-08-10 15:38:36 +0800

    fix: docker building

--- pkgs/applications/virtualization/docker/default.nix
+++ pkgs/applications/virtualization/docker/default.nix
@@ -3,9 +3,12 @@
 with lib;
 
 rec {
-  dockerGen = { version, rev, sha256, moby-src, runcRev, runcSha256
-    , containerdRev, containerdSha256, tiniRev, tiniSha256, buildxSupport ? true
-    , composeSupport ? true
+  dockerGen = {
+      version, rev, sha256
+      , moby-src
+      , runcRev, runcSha256
+      , containerdRev, containerdSha256
+      , tiniRev, tiniSha256, buildxSupport ? true, composeSupport ? true
       # package dependencies
       , stdenv, fetchFromGitHub, fetchpatch, buildGoPackage
       , makeWrapper, installShellFiles, pkg-config, glibc
@@ -39,29 +42,42 @@ rec {
         rev = containerdRev;
         sha256 = containerdSha256;
       };
-    in buildGoPackage ((optionalAttrs (!clientOnly) {
+      buildInputs = oldAttrs.buildInputs ++ [ libseccomp ];
+    });
 
-      inherit docker-runc docker-containerd docker-proxy docker-tini moby;
-
-    }) // rec {
-      inherit version rev;
-
-      pname = "docker";
-
+    docker-tini = tini.overrideAttrs  (oldAttrs: {
+      name = "docker-init-${version}";
+      inherit version;
       src = fetchFromGitHub {
-        owner = "docker";
-        repo = "cli";
-        rev = "v${version}";
-        sha256 = sha256;
+        owner = "krallin";
+        repo = "tini";
+        rev = tiniRev;
+        sha256 = tiniSha256;
       };
 
-      goPackagePath = "github.com/docker/cli";
+      # Do not remove static from make files as we want a static binary
+      postPatch = "";
 
-      nativeBuildInputs =
-        [ makeWrapper pkg-config go-md2man go libtool installShellFiles ];
-      buildInputs =
-        optionals (!clientOnly) [ sqlite lvm2 btrfs-progs systemd libseccomp ]
-        ++ plugins;
+      buildInputs = [ glibc glibc.static ];
+
+      NIX_CFLAGS_COMPILE = "-DMINIMAL=ON";
+    });
+
+    moby = buildGoPackage ((optionalAttrs (stdenv.isLinux)) rec {
+      name = "moby-${version}";
+      inherit version;
+      inherit docker-runc docker-containerd docker-proxy docker-tini;
+
+      src = moby-src;
+
+      goPackagePath = "github.com/docker/docker";
+
+      nativeBuildInputs = [ makeWrapper pkg-config go-md2man go libtool installShellFiles ];
+      buildInputs = [ sqlite lvm2 btrfs-progs systemd libseccomp ];
+
+      extraPath = optionals (stdenv.isLinux) (makeBinPath [ iproute2 iptables e2fsprogs xz xfsprogs procps util-linux git ]);
+
+      extraUserPath = optionals (stdenv.isLinux && !clientOnly) (makeBinPath [ rootlesskit slirp4netns fuse-overlayfs ]);
 
       patches = [
         # This patch incorporates code from a PR fixing using buildkit with the ZFS graph driver.
@@ -74,65 +90,42 @@ rec {
       ];
 
       postPatch = ''
-        patchShebangs man scripts/build/
-        substituteInPlace ./scripts/build/.variables --replace "set -eu" ""
-      '' + optionalString (plugins != [ ]) ''
-        substituteInPlace ./cli-plugins/manager/manager_unix.go --replace /usr/libexec/docker/cli-plugins \
-            "${pluginsRef}/libexec/docker/cli-plugins"
+        patchShebangs hack/make.sh hack/make/
       '';
 
-      # Keep eyes on BUILDTIME format - https://github.com/docker/cli/blob/${version}/scripts/build/.variables
       buildPhase = ''
         export GOCACHE="$TMPDIR/go-cache"
-
+        # build engine
         cd ./go/src/${goPackagePath}
-        # Mimic AUTO_GOPATH
-        mkdir -p .gopath/src/github.com/docker/
-        ln -sf $PWD .gopath/src/github.com/docker/cli
-        export GOPATH="$PWD/.gopath:$GOPATH"
-        export GITCOMMIT="${rev}"
+        export AUTO_GOPATH=1
+        export DOCKER_GITCOMMIT="${rev}"
         export VERSION="${version}"
-        export BUILDTIME="1970-01-01T00:00:00Z"
-        source ./scripts/build/.variables
-        export CGO_ENABLED=1
-        go build -tags pkcs11 --ldflags "$LDFLAGS" github.com/docker/cli/cmd/docker
+        ./hack/make.sh dynbinary
         cd -
       '';
 
-      outputs = [ "out" "man" ];
-
       installPhase = ''
         cd ./go/src/${goPackagePath}
-        install -Dm755 ./docker $out/libexec/docker/docker
+        install -Dm755 ./bundles/dynbinary-daemon/dockerd $out/libexec/docker/dockerd
 
-        makeWrapper $out/libexec/docker/docker $out/bin/docker \
+        makeWrapper $out/libexec/docker/dockerd $out/bin/dockerd \
           --prefix PATH : "$out/libexec/docker:$extraPath"
-      '' + optionalString (!clientOnly) ''
-        # symlink docker daemon to docker cli derivation
-        ln -s ${moby}/bin/dockerd $out/bin/dockerd
-        ln -s ${moby}/bin/dockerd-rootless $out/bin/dockerd-rootless
+
+        ln -s ${docker-containerd}/bin/containerd $out/libexec/docker/containerd
+        ln -s ${docker-containerd}/bin/containerd-shim $out/libexec/docker/containerd-shim
+        ln -s ${docker-runc}/bin/runc $out/libexec/docker/runc
+        ln -s ${docker-proxy}/bin/docker-proxy $out/libexec/docker/docker-proxy
+        ln -s ${docker-tini}/bin/tini-static $out/libexec/docker/docker-init
 
         # systemd
-        mkdir -p $out/etc/systemd/system
-        ln -s ${moby}/etc/systemd/system/docker.service $out/etc/systemd/system/docker.service
-        ln -s ${moby}/etc/systemd/system/docker.socket $out/etc/systemd/system/docker.socket
-      '' + ''
-        # completion (cli)
-        installShellCompletion --bash ./contrib/completion/bash/docker
-        installShellCompletion --fish ./contrib/completion/fish/docker.fish
-        installShellCompletion --zsh  ./contrib/completion/zsh/_docker
-      '' + lib.optionalString (stdenv.hostPlatform == stdenv.buildPlatform) ''
-        # Generate man pages from cobra commands
-        echo "Generate man pages from cobra"
-        mkdir -p ./man/man1
-        go build -o ./gen-manpages github.com/docker/cli/man
-        ./gen-manpages --root . --target ./man/man1
-      '' + ''
-        # Generate legacy pages from markdown
-        echo "Generate legacy manpages"
-        ./man/md2man-all.sh -q
+        install -Dm644 ./contrib/init/systemd/docker.service $out/etc/systemd/system/docker.service
+        substituteInPlace $out/etc/systemd/system/docker.service --replace /usr/bin/dockerd $out/bin/dockerd
+        install -Dm644 ./contrib/init/systemd/docker.socket $out/etc/systemd/system/docker.socket
 
-        installManPage man/*/*.[1-9]
+        # rootless Docker
+        install -Dm755 ./contrib/dockerd-rootless.sh $out/libexec/docker/dockerd-rootless.sh
+        makeWrapper $out/libexec/docker/dockerd-rootless.sh $out/bin/dockerd-rootless \
+          --prefix PATH : "$out/libexec/docker:$extraPath:$extraUserPath"
       '';
 
       DOCKER_BUILDTAGS = []
@@ -243,9 +236,9 @@ rec {
       platforms = with platforms; linux ++ darwin;
     };
 
-      # Exposed for tarsum build on non-linux systems (build-support/docker/default.nix)
-      inherit moby-src;
-    });
+    # Exposed for tarsum build on non-linux systems (build-support/docker/default.nix)
+    inherit moby-src;
+  });
 
   # Get revisions from
   # https://github.com/moby/moby/tree/${version}/hack/dockerfile/install/*
