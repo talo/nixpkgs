eefc04653179562965e052b42e6ed1c5bab8f5f1
Ryan Swart <ryan@talo.systems>
2022-11-07 14:21:04 +0800

    fix: ensure all python versions are overriden

--- overlay.nix
+++ overlay.nix
@@ -1,23 +1,26 @@
 _: super: let
-  python3 = super.python3.override (_: {
-    packageOverrides = super: _: {
-      feast = super.callPackage ./pkgs/development/python-modules/feast/default.nix {};
-      googledrivedownloader = super.callPackage ./pkgs/development/python-modules/googledrivedownloader/default.nix {};
-      grpcio-reflection = super.callPackage ./pkgs/development/python-modules/grpcio-reflection/default.nix {};
-      oddt = super.callPackage ./pkgs/development/python-modules/oddt/default.nix {};
-      openmm = super.callPackage ./pkgs/development/python-modules/openmm/default.nix {enableCuda = true;};
-      pdbfixer = super.callPackage ./pkgs/development/python-modules/pdfixer/default.nix {};
-      py-spy = super.callPackage ./pkgs/development/python-modules/py-spy/default.nix {};
-      pykeops = super.callPackage ./pkgs/development/python-modules/pykeops/default.nix {};
-      torch-cluster = super.callPackage ./pkgs/development/python-modules/torch-cluster/default.nix {};
-      torch-geometric = super.callPackage ./pkgs/development/python-modules/torch-geometric/default.nix {};
-      torch-scatter = super.callPackage ./pkgs/development/python-modules/torch-scatter/default.nix {};
-      torch-sparse = super.callPackage ./pkgs/development/python-modules/torch-sparse/default.nix {};
-      torch-spline-conv = super.callPackage ./pkgs/development/python-modules/torch-spline-conv/default.nix {};
-    };
+  packageOverrides = super: _: {
+    feast = super.callPackage ./pkgs/development/python-modules/feast/default.nix {};
+    googledrivedownloader = super.callPackage ./pkgs/development/python-modules/googledrivedownloader/default.nix {};
+    grpcio-reflection = super.callPackage ./pkgs/development/python-modules/grpcio-reflection/default.nix {};
+    oddt = super.callPackage ./pkgs/development/python-modules/oddt/default.nix {};
+    openmm = super.callPackage ./pkgs/development/python-modules/openmm/default.nix {enableCuda = true;};
+    pdbfixer = super.callPackage ./pkgs/development/python-modules/pdfixer/default.nix {};
+    py-spy = super.callPackage ./pkgs/development/python-modules/py-spy/default.nix {};
+    pykeops = super.callPackage ./pkgs/development/python-modules/pykeops/default.nix {};
+    torch-cluster = super.callPackage ./pkgs/development/python-modules/torch-cluster/default.nix {};
+    torch-geometric = super.callPackage ./pkgs/development/python-modules/torch-geometric/default.nix {};
+    torch-scatter = super.callPackage ./pkgs/development/python-modules/torch-scatter/default.nix {};
+    torch-sparse = super.callPackage ./pkgs/development/python-modules/torch-sparse/default.nix {};
+    torch-spline-conv = super.callPackage ./pkgs/development/python-modules/torch-spline-conv/default.nix {};
+  };
+  python310 = super.python310.override (_: {
+    inherit packageOverrides;
   });
-in
-  {
+  python3 = super.python3.override (_: {
+    inherit packageOverrides;
+  });
+in {
   autodock-vina = super.callPackage ./pkgs/applications/science/autodock-vina {};
   p2rank = super.callPackage ./pkgs/applications/science/p2rank {};
   reduce = super.callPackage ./pkgs/applications/science/reduce {};
@@ -28,5 +31,8 @@ in
   # openmm = super.callPackage ./pkgs/development/libraries/openmm {};
 
   inherit python3;
+
+  pythonPackages = python310.pkgs;
   python3Packages = python3.pkgs;
+  python310Packages = python310.pkgs;
 }
